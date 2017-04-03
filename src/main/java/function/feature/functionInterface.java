61e6eaf530c2411ebc19e3c196473a539f164dbf
乔凯 <qiaokai@thinkit.cn>
2017-04-03 11:19:29 +0800

    function interface

---
+++ src/main/java/function/feature/functionInterface.java
@@ -0,0 +1,17 @@
+package function.feature;
+
+/**
+ * Created by joqk12345 on 4/3/17.
+ * www.github.com/joqk12345
+ */
+
+/**
+ * only one function interface
+ */
+@FunctionalInterface
+public interface functionInterface {
+    /**
+     * Method.
+     */
+    void method();
+}
